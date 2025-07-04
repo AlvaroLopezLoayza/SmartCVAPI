aaef90c6bcff6cfd4ff917dbfa1a2537a2a4710a
AlvaroLopezLoayza <40212708+AlvaroLopezLoayza@users.noreply.github.com>
2025-07-04 14:52:36 +0000

    conn string fix

--- api/db.js
+++ api/db.js
@@ -1,6 +1,11 @@
-import postgres from 'postgres';
-const sql = postgres(connectionString, {
-  ssl: 'require'
-});
+import postgres from 'postgres'
+
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL no está definida en las variables de entorno")
+}
+
+const sql = postgres(connectionString, { ssl: 'require' })
 
 export default sql
